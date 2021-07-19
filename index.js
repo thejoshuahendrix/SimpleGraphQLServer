cfe98d3c955db414faa8c4eef71f1df041814473
thejoshuahendrix <56802866+thejoshuahendrix@users.noreply.github.com>
2021-07-19 11:08:59 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+/* index.js */
+
+const express = require("express");
+const {graphqlHTTP} = require("express-graphql");
+const schema = require("./schema");
+const resolvers = require("./resolvers");
+
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: resolvers,
+    graphiql: true
+  })
+);
+
+
+const port = process.env.PORT || 4000;
+
+app.listen(port);
+
+console.log(`🚀 Server ready at http://localhost:${port}/graphql`);
